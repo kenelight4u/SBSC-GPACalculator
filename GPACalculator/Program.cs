d7fb689be55aaa1917d88efd790f8d18129f9787
Anolue Ekene Samuel <kenelight4u@gmail.com>
2021-05-03 03:29:08 +0100

    implemented the Calculator Class

--- GPACalculator/Program.cs
+++ GPACalculator/Program.cs
@@ -85,7 +85,10 @@ namespace GPACalculator
                 while (menu.GetCurrentStatge() == 3)
                 {
                     var myData = appDb.GetAllCourses();
-                   
+                    
+                    List<Course> myH = new List<Course>();
+                    myH = (List<Course> )myData;
+
                     // myData.Count() == 0
                     if (!myData.Any())
                     {
@@ -95,13 +98,13 @@ namespace GPACalculator
                     }
                     else
                     {
-                        Menu.PromptUser($"The Data inputted are: ");
-                        Menu.PromptUser($"{"Course Code", -15} | {"Units", 5} | {"Score", 5}");
-                        foreach (var data in myData)
-                        {
-                            Menu.PromptUser($"{data.courseCode.ToUpper(), -15} {data.numberOfUnits, 5} {data.courseScore, 7}");
-                        }
-                        // GPACal.Calculator(List<Course> appDb);
+                        // Menu.PromptUser($"The Data inputted are: ");
+                        // Menu.PromptUser($"{"Course Code", -15} | {"Units", 5} | {"Score", 5}");
+                        // foreach (var data in myData)
+                        // {
+                        //     Menu.PromptUser($"{data.courseCode.ToUpper(), -15} {data.numberOfUnits, 5} {data.courseScore, 7}");
+                        // }
+                        GPACal.ListCourse(myH);
                         
 
                         Menu.PromptUser($" ");
@@ -119,49 +122,50 @@ namespace GPACalculator
 
                         if (yesOrNO == "yes")
                         {
-                            double unitsTimesGradePoint = 0d; int sumUnits = 0; decimal gpa = 0M;
-                            Grades myGrade; int myGradePoint;
+                            GPACal.Calculator(myH);
+                            // double unitsTimesGradePoint = 0d; int sumUnits = 0; decimal gpa = 0M;
+                            // Grades myGrade; int myGradePoint;
 
-                            Menu.PromptUser($"The Data inputted are: ");
-                            Menu.PromptUser($"{"Course Code", -15} | {"Units", 5} | {"Score", 5} | {"Grade", 5}");
-                            foreach (var value in myData)
-                            {
+                            // Menu.PromptUser($"The Data inputted are: ");
+                            // Menu.PromptUser($"{"Course Code", -15} | {"Units", 5} | {"Score", 5} | {"Grade", 5}");
+                            // foreach (var value in myData)
+                            // {
                             
-                                if (value.courseScore >= 70 && value.courseScore < 100)
-                                {
-                                    myGrade = Grades.A;
-                                    myGradePoint = (int) Grades.A;
-                                }
-                                else if (value.courseScore >= 60 && value.courseScore < 70)
-                                {
-                                    myGrade = Grades.B;
-                                    myGradePoint = (int) Grades.B;
-                                }
-                                else if (value.courseScore >= 50 && value.courseScore < 60)
-                                {
-                                    myGrade = Grades.C;
-                                    myGradePoint = (int) Grades.C;
-                                }
-                                else if (value.courseScore >= 40 && value.courseScore < 50)
-                                {
-                                    myGrade = Grades.D;
-                                    myGradePoint = (int) Grades.D;
-                                }
-                                else
-                                {
-                                    myGrade = Grades.F;
-                                    myGradePoint = (int) Grades.F;
-                                }
+                            //     if (value.courseScore >= 70 && value.courseScore < 100)
+                            //     {
+                            //         myGrade = Grades.A;
+                            //         myGradePoint = (int) Grades.A;
+                            //     }
+                            //     else if (value.courseScore >= 60 && value.courseScore < 70)
+                            //     {
+                            //         myGrade = Grades.B;
+                            //         myGradePoint = (int) Grades.B;
+                            //     }
+                            //     else if (value.courseScore >= 50 && value.courseScore < 60)
+                            //     {
+                            //         myGrade = Grades.C;
+                            //         myGradePoint = (int) Grades.C;
+                            //     }
+                            //     else if (value.courseScore >= 40 && value.courseScore < 50)
+                            //     {
+                            //         myGrade = Grades.D;
+                            //         myGradePoint = (int) Grades.D;
+                            //     }
+                            //     else
+                            //     {
+                            //         myGrade = Grades.F;
+                            //         myGradePoint = (int) Grades.F;
+                            //     }
 
-                                unitsTimesGradePoint += myGradePoint * value.numberOfUnits;
-                                sumUnits += value.numberOfUnits;
-                                Menu.PromptUser($"{value.courseCode.ToUpper(), -15} {value.numberOfUnits, 5} {value.courseScore, 7} {myGrade, 7}");
-                            }
-                            gpa = Convert.ToDecimal(unitsTimesGradePoint)  / sumUnits;
-                            Menu.PromptUser($"GPA is: {gpa}");
+                            //     unitsTimesGradePoint += myGradePoint * value.numberOfUnits;
+                            //     sumUnits += value.numberOfUnits;
+                            //     Menu.PromptUser($"{value.courseCode.ToUpper(), -15} {value.numberOfUnits, 5} {value.courseScore, 7} {myGrade, 7}");
+                            // }
+                            // gpa = Convert.ToDecimal(unitsTimesGradePoint)  / sumUnits;
+                            // Menu.PromptUser($"GPA is: {gpa}");
 
-                            Console.ReadKey();
-                            Console.Clear();
+                            // Console.ReadKey();
+                            // Console.Clear();
                             // appDb.RemoveAllCourses();
                             menu.SetCurrentStage(1);   
                         }
